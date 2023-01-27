e7ea3e83367ee158c5709818b4fe70ed3a07609e
liuxingang <lxg13171827320@163.com>
2023-01-27 20:35:19 +0800

    改用 css 变量而非写死的数值

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 /* @refresh reload */
 import { render } from 'solid-js/web';
 
-import './index.css';
+import './style/index.less';
 import App from './App';
 
 render(() => <App />, document.getElementById('root') as HTMLElement);
